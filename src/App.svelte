cc7dab6e748fb19e382ef562db3efed6fad15ff1
iseau395 <70857830+iseau395@users.noreply.github.com>
2022-08-21 07:48:51 -0700

    Add scrolling space background

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,40 @@
 <script lang="ts">
+import { onMount, onDestroy } from "svelte";
 
+
+    let backgound_x = 0;
+    let backgound_y = 0;
+
+    let animation_frame = null;
+
+    onMount(() => {
+        function tick_animation() {
+            backgound_x += 0.05;
+            backgound_y += 0.05;
+
+            backgound_x = backgound_x % 512;
+            backgound_y = backgound_y % 512;
+
+            animation_frame = requestAnimationFrame(tick_animation);
+        }
+
+        tick_animation();
+    });
+
+    onDestroy(() => {
+        cancelAnimationFrame(animation_frame);
+    })
 </script>
+
+<main style="background-position: {Math.floor(backgound_x)}px {Math.floor(backgound_y)}px">
+
+</main>
+
+<style>
+    main {
+        background-image: url("/media/space.png");
+
+        width: 100%;
+        height: 100%;
+    }
+</style>
